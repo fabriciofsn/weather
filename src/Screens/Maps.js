ad62e600245f882cecf1f5e1a5e2e17a79053ee8
Fabricio souza <banksapk@gmail.com>
2023-03-11 00:50:59 -0300

    removed

--- src/Screens/Maps.js
+++ src/Screens/Maps.js
@@ -3,19 +3,8 @@ import { GlobalContext } from "../hooks/GlobalContext";
 import { DivWraper } from "../styles/Maps";
 
 const Maps = ({ data }) => {
-  const { response } = React.useContext(GlobalContext);
-  if (data) console.log(data);
-  return (
-    <DivWraper>
-      <div>
-        <span>Vento: {data.wind.speed}Km</span> |
-        <span>Umidade: {data.main.humidity}</span>
-      </div>
-      <div>
-        <span>Nível do Mar: {data.main.sea_level}</span>
-      </div>
-    </DivWraper>
-  );
+  // const { response } = React.useContext(GlobalContext);
+  if (data) return <DivWraper></DivWraper>;
 };
 
 export default Maps;
